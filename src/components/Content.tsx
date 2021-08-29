58df8df6dd657e0f55be588bb93162640b1e53d1
EddyPBR <edvaldojunior1310@gmail.com>
2021-08-29 10:35:55 -0300

    :tada: challenge completed

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,22 @@
-export function Content() {
-  // Complete aqui
+import { ReactNode } from "react";
+
+interface SideBarProps {
+  children: ReactNode;
+  title: string;
+}
+
+export function Content({ children, title }: SideBarProps) {
+  return(
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {children}
+        </div>
+      </main>
+    </div>
+  )
 }
